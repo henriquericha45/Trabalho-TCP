a1f6cfc16523cbafd10867544cca3bac7bd24869
henriquericha45 <henriquericha@hotmail.com>
2021-11-18 09:46:27 -0300

    retirando warning

--- Trabalhador.java
+++ Trabalhador.java
@@ -1,6 +1,4 @@
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.ObjectInputStream;
